54d121dc8ee11dd3c2f9ab54657595282f6e1fa8
Ryan-Burgess8060 <71049916+Ryan-Burgess8060@users.noreply.github.com>
2021-04-29 03:12:02 -0400

    Add files via upload

--- auto.py
+++ auto.py
@@ -20,8 +20,8 @@ if response.getcode() >= 200 and response.getcode() < 300:
 		print("Redirected to " + response.geturl())
 	html_doc = response.read()
 	soup = BeautifulSoup(html_doc, 'html.parser')
-	for link in soup.find_all('meta'): # It helps to find all anchor tag's
-		print(link.get('author'))
+	for link in soup.find_all('form'): # It helps to find all anchor tag's
+		print(link.get('method'))
 # Note: urllib.urlopen automatically follows redirects (codes 300-399)
 elif response.getcode() >= 400 and response.getcode() < 500:
 	print(str(response.getcode()) + " - Client error")
@@ -34,4 +34,114 @@ elif response.getcode() >= 500 and response.getcode() < 600:
 else:
 	print(str(response.getcode()))
 
+url = "https://christopherburgess.xyz/WebSecurity/login.php"
+print("Opening " + url)
+start = time.clock()
+response = urllib.urlopen(url)
+elapsed = time.clock() - start
+print("Response code: " + str(response.getcode()))
+print("Response time: " + str(elapsed))
 
+# Check the status code and print results
+if response.getcode() >= 200 and response.getcode() < 300:
+	if response.geturl() != url:
+		print("Redirected to " + response.geturl())
+	html_doc = response.read()
+	soup = BeautifulSoup(html_doc, 'html.parser')
+	for link in soup.find_all('form'): # It helps to find all anchor tag's
+		print(link.get('method'))
+# Note: urllib.urlopen automatically follows redirects (codes 300-399)
+elif response.getcode() >= 400 and response.getcode() < 500:
+	print(str(response.getcode()) + " - Client error")
+	html_doc = response.read()
+	print(html_doc)
+elif response.getcode() >= 500 and response.getcode() < 600:
+	print(str(response.getcode()) + " - Server error")
+	html_doc = response.read()
+	print(html_doc)
+else:
+	print(str(response.getcode()))
+
+url = "https://christopherburgess.xyz/WebSecurity/password.php"
+print("Opening " + url)
+start = time.clock()
+response = urllib.urlopen(url)
+elapsed = time.clock() - start
+print("Response code: " + str(response.getcode()))
+print("Response time: " + str(elapsed))
+
+# Check the status code and print results
+if response.getcode() >= 200 and response.getcode() < 300:
+	if response.geturl() != url:
+		print("Redirected to " + response.geturl())
+	html_doc = response.read()
+	soup = BeautifulSoup(html_doc, 'html.parser')
+	for link in soup.find_all('form'): # It helps to find all anchor tag's
+		print(link.get('method'))
+# Note: urllib.urlopen automatically follows redirects (codes 300-399)
+elif response.getcode() >= 400 and response.getcode() < 500:
+	print(str(response.getcode()) + " - Client error")
+	html_doc = response.read()
+	print(html_doc)
+elif response.getcode() >= 500 and response.getcode() < 600:
+	print(str(response.getcode()) + " - Server error")
+	html_doc = response.read()
+	print(html_doc)
+else:
+	print(str(response.getcode()))
+
+url = "https://christopherburgess.xyz/WebSecurity/register.php"
+print("Opening " + url)
+start = time.clock()
+response = urllib.urlopen(url)
+elapsed = time.clock() - start
+print("Response code: " + str(response.getcode()))
+print("Response time: " + str(elapsed))
+
+# Check the status code and print results
+if response.getcode() >= 200 and response.getcode() < 300:
+	if response.geturl() != url:
+		print("Redirected to " + response.geturl())
+	html_doc = response.read()
+	soup = BeautifulSoup(html_doc, 'html.parser')
+	for link in soup.find_all('form'): # It helps to find all anchor tag's
+		print(link.get('method'))
+# Note: urllib.urlopen automatically follows redirects (codes 300-399)
+elif response.getcode() >= 400 and response.getcode() < 500:
+	print(str(response.getcode()) + " - Client error")
+	html_doc = response.read()
+	print(html_doc)
+elif response.getcode() >= 500 and response.getcode() < 600:
+	print(str(response.getcode()) + " - Server error")
+	html_doc = response.read()
+	print(html_doc)
+else:
+	print(str(response.getcode()))
+
+url = "https://christopherburgess.xyz/WebSecurity/topic.php"
+print("Opening " + url)
+start = time.clock()
+response = urllib.urlopen(url)
+elapsed = time.clock() - start
+print("Response code: " + str(response.getcode()))
+print("Response time: " + str(elapsed))
+
+# Check the status code and print results
+if response.getcode() >= 200 and response.getcode() < 300:
+	if response.geturl() != url:
+		print("Redirected to " + response.geturl())
+	html_doc = response.read()
+	soup = BeautifulSoup(html_doc, 'html.parser')
+	for link in soup.find_all('form'): # It helps to find all anchor tag's
+		print(link.get('method'))
+# Note: urllib.urlopen automatically follows redirects (codes 300-399)
+elif response.getcode() >= 400 and response.getcode() < 500:
+	print(str(response.getcode()) + " - Client error")
+	html_doc = response.read()
+	print(html_doc)
+elif response.getcode() >= 500 and response.getcode() < 600:
+	print(str(response.getcode()) + " - Server error")
+	html_doc = response.read()
+	print(html_doc)
+else:
+	print(str(response.getcode()))
